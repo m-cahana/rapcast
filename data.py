9206d8796385405cb3ae3604cf000ca873efe3b5
Maxine Whitely <m.whitely16@gmail.com>
2018-06-05 21:08:21 -0500

    starting to generate data with 10-fold cv

--- data.py
+++ data.py
@@ -1,18 +1,24 @@
 import json
 from collections import Counter
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import chi2
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
-import numpy as np
-import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix
+
 
 rappers = pd.read_csv('./input/rappers.csv')
-rappers.Name = rappers.Name.str.lower()
-rappers.columns = ['artist', 'era', 'city', 'sub_city', 'region']
+rappers.artist = rappers.artist.str.lower()
 rappers.region = rappers.region.replace('Northeast ', 'Northeast')
 rappers.region = rappers.region.replace('New Jersey', 'Northeast')
 rappers.region = rappers.region.replace('Southeast ', 'Southeast')
@@ -93,24 +99,13 @@ tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 clf = MultinomialNB().fit(X_train_tfidf, y_train)
 
-X_train, X_test, y_train, y_test = train_test_split(df['corpus'], df[location], random_state = 0)
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(X_train)
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-clf = MultinomialNB().fit(X_train_tfidf, y_train)
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import LinearSVC
-from sklearn.model_selection import cross_val_score
 models = [
     RandomForestClassifier(n_estimators=200, max_depth=3, random_state=0),
     LinearSVC(),
     MultinomialNB(),
     LogisticRegression(random_state=0),
 ]
-CV = 5
+CV = 10
 cv_df = pd.DataFrame(index=range(CV * len(models)))
 entries = []
 for model in models:
@@ -119,7 +114,6 @@ for model in models:
   for fold_idx, accuracy in enumerate(accuracies):
     entries.append((model_name, fold_idx, accuracy))
 cv_df = pd.DataFrame(entries, columns=['model_name', 'fold_idx', 'accuracy'])
-import seaborn as sns
 sns.boxplot(x='model_name', y='accuracy', data=cv_df)
 sns.stripplot(x='model_name', y='accuracy', data=cv_df,
               size=8, jitter=True, edgecolor="gray", linewidth=2)
@@ -129,7 +123,6 @@ model = LinearSVC()
 X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.33, random_state=0)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
-from sklearn.metrics import confusion_matrix
 conf_mat = confusion_matrix(y_test, y_pred)
 fig, ax = plt.subplots(figsize=(10,10))
 sns.heatmap(conf_mat, annot=True, fmt='d',
@@ -137,3 +130,6 @@ sns.heatmap(conf_mat, annot=True, fmt='d',
 plt.ylabel('Actual')
 plt.xlabel('Predicted')
 plt.savefig('./figures/'+ str(era) + str(location) + '_matrix.pdf')
+
+with open('./output/' + str(era) + str(location) + '.json', 'w') as outfile:
+    json.dump(model.coef_.tolist(), outfile)
